0a5e843077a00245ff7a473bb1c28302220ab255
jessegutierrez <jesse0707@gmail.com>
2021-10-26 16:39:21 -0500

    change app text and new branch called Feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOLA MUNDO!
+          Cambien al branch de "Feature"
         </p>
         <a
           className="App-link"
